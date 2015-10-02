929062aa676555d9acc96ccb29c1535c301d231c
Theo Baes <baes.theo@gmail.com>
2015-10-02 13:59:08 +0200

    Module structure + web server

--- js/game/gameMap.js
+++ js/game/gameMap.js
@@ -2,6 +2,10 @@
 
 function GameMap() {
     this.map = null;
+    this.entities = {
+        player: null,
+        ladders: []
+    };
 
     this.load();
 }
@@ -13,15 +17,59 @@ GameMap.prototype.load = function() {
     xhr.onload = function() {
         try {
             self.map = JSON.parse(xhr.responseText);
-            console.log(self);
+            self.parse();
         }
         catch(err) {
             console.error(err);
         }
     };
 
-    xhr.withCredentials = true;
-
     xhr.open('GET', '/map.json');
     xhr.send(null);
 };
+
+GameMap.prototype.parse = function() {
+
+    for(var y = 0; y < this.map.length; y++) {
+
+        for(var x = 0; x < this.map[y].length; x++) {
+
+            if(this.caseType(x, y) == 'Player') {
+                console.log('P', x, y);
+            }
+
+        }
+
+    }
+
+};
+
+GameMap.prototype.caseType = function(x, y) {
+
+    if(!this.map[y][x]) return null;
+
+    switch(this.map[y][x]) {
+
+        case 0:
+            return 'Case';
+        break;
+
+        case 1:
+            return 'Ladder';
+        break;
+
+        case 2:
+            return 'Player';
+        break;
+
+        case 3:
+            return 'Enemy';
+        break;
+
+        case 4:
+            return 'Barrel';
+        break;
+
+    }
+
+}
--- js/game/player.js
+++ js/game/player.js
@@ -1,5 +1,19 @@
 "use strict";
 
 function Player() {
-
+    this.x = 1; //TODO: x,y from the map.json
+    this.y = 13; //TODO: x,y from the map.json
+    this.height = 2; //TODO: height from the map.json
 }
+
+Player.prototype.move = function(x, y) {
+    this.x = x;
+    this.y = y;
+};
+
+Player.prototype.moveFrom = function(xDiff, yDiff) {
+    this.move(
+        this.x + xDiff,
+        this.y + yDiff
+    );
+};
